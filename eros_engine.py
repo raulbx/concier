13758a1c8c6cd2eb33687d49b1c98810601eaba9
Rahul <Rahul@kalpa.com>
2018-02-10 15:30:05 -0500

    with fb code

--- eros_engine.py
+++ eros_engine.py
@@ -1,10 +1,10 @@
 user_state = {
 	'recipient_id':'',
 	'fbseq':0,
-	'active_question':0,
+	'active_question':1,
 	'responder_id':''
 }
-responses = ['This is Eros Match. How can I help you?','Let me find someone, who can help you with it.',' is happy to help you with this purchase. Do you want to connect with him?','Great. I am going to connect you to ']
+responses = ['This is Eros Match. How can I help you?','Let me find someone, who can help you with it.','Rob happy to help you with this purchase. Do you want to connect with him?','Great. I am going to connect you to ']
 def fbrespond(recipient_id,sequence):
 	response_message = 'This is empty'
 	if user_state['recipient_id']== recipient_id :
@@ -16,7 +16,7 @@ def fbrespond(recipient_id,sequence):
 			if user_state['active_question'] < 3 :
 				user_state['active_question'] = user_state['active_question'] + 1
 			else :
-				user_state['active_question'] = 0
+				user_state['active_question'] = 1
 			nextQID = user_state['active_question']
 			print (user_state)
 			response_message=responses[nextQID]
