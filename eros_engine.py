15700d6ec63b4af90a5b3c9438b9cf49c25629d3
Rahul <Rahul@kalpa.com>
2018-02-10 15:16:14 -0500

    with fb code

--- eros_engine.py
+++ eros_engine.py
@@ -1,17 +1,24 @@
 user_state = {
 	'recipient_id':'',
-	'question':0,
+	'fbseq':0,
+	'active_question':0,
 	'responder_id':''
 }
-responses = ['This is Eros. How can I help you?','Let me find someone, who can help you with it.',' is happy to help you with this purchase. Do you want to connect with him?','Great. I am going to connect you to ']
-def fbrespond(recipient_id):
+responses = ['This is Eros Match. How can I help you?','Let me find someone, who can help you with it.',' is happy to help you with this purchase. Do you want to connect with him?','Great. I am going to connect you to ']
+def fbrespond(recipient_id,sequence):
 	response_message = 'This is empty'
 	if user_state['recipient_id']== recipient_id :
 		print("User interaction is in progress")
-		nextQuestion=user_state['question']+1
-		response_message=responses[nextQuestion]
-		user_state['question']=nextQuestion
-		return response_message
+		if sequence > user_state['fbseq'] :
+			#This is new message. Advance to the next question
+			user_state['fbseq']=sequence
+			user_state['active_question'] = user_state['active_question'] + 1
+			nextQID = user_state['active_question']
+			print (user_state)
+			response_message=responses[nextQID]
+			return response_message
+		else :
+			print ('Message came twice')
 	# they are already in conversation
 	else :
 		print ('First interaction')
