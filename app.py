12f8f09843770893ca3023735ac88d125096209d
Rahul <Rahul@kalpa.com>
2018-02-10 15:17:39 -0500

    with fb code

--- app.py
+++ app.py
@@ -22,16 +22,15 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
        output = request.get_json()
-       print (output)
+       #print (output)
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
-                #print(recipient_id)
                 if message['message'].get('text'):
-                    response_sent_text = eros_engine.fbrespond(recipient_id)
+                    response_sent_text = eros_engine.fbrespond(recipient_id, message['message'].get('seq'))
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
