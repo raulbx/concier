124aa5e3e268ac5754cd8309361cb056a149430a
Rahul <Rahul@kalpa.com>
2018-02-10 13:00:14 -0500

    with fb code

--- app.py
+++ app.py
@@ -22,13 +22,14 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
        output = request.get_json()
+       print (output)
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
-                print(recipient_id)
+                #print(recipient_id)
                 if message['message'].get('text'):
                     response_sent_text = bot_engine.respond()
                     send_message(recipient_id, response_sent_text)
