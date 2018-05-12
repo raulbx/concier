547107dba02a55fd99ff674181481d2165d64e1b
Rahul <Rahul@kalpa.com>
2018-05-11 23:25:53 -0400

    Adding firestone

--- core_engine.py
+++ core_engine.py
@@ -32,13 +32,12 @@ def fbrespond(recipient_id,sequence):
 		response_message = responses[1]
 	return response_message
 
-def checkDB():
-	firebase_config = ast.literal_eval(os.environ["FIREBASE_CONFIG"])
-	cred = credentials.Certificate(firebase_config)
+def checkDB(): 
+	cred = credentials.Certificate(ast.literal_eval(os.environ["FIREBASE_CONFIG"]))
 	firebase_admin.initialize_app(cred)
 	db = firestore.client()
 	fb_id='16093421424752504'
-	query_ref = db.collection(u'members').where(u'fb_id', u'==', self.fb_id)
+	query_ref = db.collection(u'members').where(u'fb_id', u'==', fb_id)
 	member_obj = None
 	members = query_ref.get()
 	for member in members:
