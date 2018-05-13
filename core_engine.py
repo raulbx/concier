4fae30f1e993ea788528c6fd9bdf2140aa6d6e60
Rahul <Rahul@kalpa.com>
2018-05-13 11:54:16 -0400

    Adding firestone

--- core_engine.py
+++ core_engine.py
@@ -16,11 +16,11 @@ class Members(object):
 		self.source = source
 
 	def find_member(self):
-    	db = firestore.client()
-    	query_ref = db.collection(u'members').where(u'fb_id', u'==', self.fb_id)
-    	#member_obj = None
-    	try:
-    		members = query_ref.get()
+		db = firestore.client()
+		query_ref = db.collection(u'members').where(u'fb_id', u'==', self.fb_id)
+		#member_obj = None
+		try:
+			members = query_ref.get()
     		for member in members:
     			member_obj = db.collection(u'members').document(member.id)
     			print ('Found Member')
@@ -29,35 +29,6 @@ class Members(object):
     	except:
     		print(u'No such document!')
     	return member_obj
-
-    def add_member(self):
-    	#if not member_exists:
-    		member_data = {
-    		'fb_id': self.fb_id,
-    		'source': self.source,
-    		'is_helper':False,
-    		'is_helpee':True,
-    		'token_balance':0,
-    		'conversations':[], # Good idea is to store references in this array
-    		'expertise':['electronics','health'],
-    		'signupdate':datetime.datetime.now(),
-    		'lastactivedate':datetime.datetime.now()
-    		}
-    		print("Added Member")
-    		return db.collection(u'members').add(member_data)
-    		#db.collection(u'members').document(u'member_00001').set(data)
-
-    def add_conversation(self,helpee_ref):
-    	conversation_data = {
-    	'helpee_ref':helpee_ref, # this is reference to the helper obj
-    	#'helpee_id':helpee_id,
-    	'active':True,
-    	'conversation_state':0,
-    	'current_chat_seq':0,
-    	'startdate':datetime.datetime.now(),
-    	'lastactivedate':datetime.datetime.now()
-    	}
-    	return db.collection(u'conversations').add(conversation_data)
 
 def checkDB():
 	cred = credentials.Certificate(ast.literal_eval(os.environ["FIREBASE_CONFIG"]))
