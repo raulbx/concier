d224c4b92490b63f94d0b802b3b3d65a4bf6deb8
Rahul <Rahul@kalpa.com>
2018-05-14 22:42:32 -0400

    Adding firestone

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ def receive_message():
                 member_Obj=core_engine.Members(placeHolderFbId)
                 member = member_Obj.get_member()
                 print(member.get().to_dict().get('fb_id'))
-                active_messages = member_Obj.get_active_conversation(member)
-                print("No of active messages ".format(active_messages))
-                sender_msg = sender_msg.format(active_messages)
+                active_count = member_Obj.get_active_conversation(member)
+                print(active_count)
+                #sender_msg = sender_msg.format(active_messages)
                 '''
                 Member found or added. Check the converstation. If there is no conversation, then this person is either an expert signing up or a new person needing help.
                 Ask them if they need help? If yes, then mark them as helpee, and start the conversation thread. 
