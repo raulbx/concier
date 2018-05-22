c5a23728472bc0e859ebeb68a67f9bd35b8b9603
Rahul <Rahul@kalpa.com>
2018-05-22 11:04:31 -0400

    Adding the conversation code

--- app.py
+++ app.py
@@ -177,6 +177,11 @@ def form_payload(response_type,text_message,recipient_id):
                     "type":"postback",
                     "title":"Household Items",
                     "payload":"house_hold_items"
+                    },
+                    {
+                    "type":"postback",
+                    "title":"Other Item",
+                    "payload":"oter_item"
                     }
                     ]
                 }
