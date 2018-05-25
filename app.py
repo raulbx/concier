487458b40774eeaf77686d2a3ac266602bd23f53
Rahul <Rahul@kalpa.com>
2018-05-25 14:37:17 -0400

    Adding the conversation code

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def receive_message():
                         # Get all the experts for this expertise 
                         query_results = member_ref.get_experts('electronics').get()
                         for result in query_results:
-                            print('ID {} and {}'.format(result.id, result.to_dict()['member']))
+                            print('ID {} and {}'.format(result.id, result.to_dict()['member'].get().to_dict()['fb_id']))
                         #se
                     #print(conversation.to_dict()['helper_ref'].get().to_dict()['fb_id'])
                 #print(payload)
