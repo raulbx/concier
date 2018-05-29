33504f9bad9fd74df04d7fbd4614996bd5256c59
Rahul <Rahul@kalpa.com>
2018-05-29 17:03:15 -0400

    Adding the conversation code

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def receive_message():
                         # Get all the experts for this expertise 
                         query_results = member_ref.get_experts(conversation.to_dict().get('product_category')).get()
                         for result in query_results:
-                            print(result.to_dict()['member'].get())
+                            print(result.to_dict()['member'])
                             #for expert_ref in result.to_dict()['member']:
                              #   print ('Expert Reference is: '.format(expert_ref.get()))
                                 #print('ID {} and {}'.format(result.id, result.to_dict()['member']))
