217554768390d66ddc2f4fa3f0aed36031931b9a
Rahul <Rahul@kalpa.com>
2018-06-15 12:52:38 -0400

    Adding the conversation code

--- core_engine.py
+++ core_engine.py
@@ -88,8 +88,8 @@ class Members(object):
 		else:
 			conversation_ref = None
 		# THERE IS SOME BUG IN PYTHON. This print statement doesn't return values from the call
-		#conversation_id = 
-		print ('This is the ID: '.format(conversation_ref.get().id))
+		conversation_id = ' '+ conversation_ref.get().id
+		print (conversation_id)
 		return conversation_ref
 
 	def get_active_conversation_ref_byID(self,conversation_id):
