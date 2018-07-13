d87c177b220b632113df9257b6dbcaba6e14ad84
Rahul <Rahul@kalpa.com>
2018-07-13 15:14:45 -0400

    Adding the conversation code

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -5,20 +5,25 @@ payload = {}
 class Exchange(object):
 
 	def __init__(self, member_identifier, source,member_core_engine_ref):
-		self.member_identifier = member_identifier
+		self.platform_id = member_identifier
 		self.source = source
 		self.member_core_engine_ref=member_core_engine_ref
 
 	def get_action(self, conversation_ref):
-		print("Member Identifier is {} and conversation_ref is {}".format(self.member_identifier,conversation_ref.get().id))
+		print("Member Identifier is {} and conversation_ref is {}".format(self.platform_id,conversation_ref.get().id))
 		sender_msg = "This is new"
-		return form_payload('plain_message',sender_msg,self.member_identifier,"conversation_id")
+		return form_payload('plain_message',sender_msg,self.platform_id,"conversation_id")
 
 	def start_conversation(self,member_ref):
-		sender_msg = "This is start of a new conversation"
 		#conversation_ref = member_ref.add_conversation(member_ref.get_member())
 		flow_state_ref = self.member_core_engine_ref.get_conv_flow_state("start_here")
-		return form_payload('plain_message',flow_state_ref.get().to_dict().get('response'),self.member_identifier,"conversation_id")
+		response = flow_state_ref.get().to_dict().get('response')
+		response_type = flow_state_ref.get().to_dict().get('response_type')
+		recipient = None 
+		if flow_state_ref.get().to_dict().get('response_type')== 'recipient':
+			recipient = platform_id
+ 
+		return form_payload(response_type,response,recipient,"conversation_id")
 
 def form_payload(response_type,text_message,recipient_id,conversation_id):
 	payload['recipient'] = {
