0addd03271b4ceee3a0884201a6d800410796ca0
Rahul <Rahul@kalpa.com>
2018-07-13 17:01:14 -0400

    Adding the conversation code

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -4,9 +4,9 @@ payload = {}
 
 class Exchange(object):
 
-	def __init__(self, member_identifier, source,member_core_engine_ref):
+	def __init__(self, member_identifier, source_platform,member_core_engine_ref):
 		self.user_id_on_platform = member_identifier
-		self.source = source
+		self.source_platform = source_platform
 		self.member_core_engine_ref=member_core_engine_ref
 
 	def get_action(self, conversation_ref):
@@ -15,14 +15,13 @@ class Exchange(object):
 		return form_payload('plain_message',sender_msg,self.user_id_on_platform,"conversation_id")
 
 	def start_conversation(self,member_ref):
-		#conversation_ref = member_ref.add_conversation(member_ref.get_member())
+		conversation_ref = member_ref.add_conversation(member_ref.get_member())
 		flow_state_ref = self.member_core_engine_ref.get_conv_flow_state("start_here")
 		response_type = flow_state_ref.get().to_dict().get('response_type')
 		response = flow_state_ref.get().to_dict().get('response')
 		recipient = None 
 		if flow_state_ref.get().to_dict().get('recipient')== 'sender':
 			recipient = self.user_id_on_platform 
-		print ("Hitting the start conversation ID is {}".format(recipient))
 		return form_payload(response_type,response,recipient,"conversation_id")
 
 def form_payload(response_type,response,recipient_id,conversation_id):
@@ -36,6 +35,25 @@ def form_payload(response_type,response,recipient_id,conversation_id):
 		}
 	elif response_type =='welcome_buttons':
 		payload['message'] = {
-		}
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text":"Hey there! How can I help?",
+                    "buttons":[
+                    {
+                    "type":"postback",
+                    "title":"Get shopping advice?",
+                    "payload":"seekingAdvice"
+                    },
+                    {
+                    "type":"postback",
+                    "title":"Other?",
+                    "payload":"other"
+                    }
+                    ]
+                }
+            }
+        }
 
 	return payload
