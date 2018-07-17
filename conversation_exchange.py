ef1913d8cedc37a227d3b71c857d37ef3f728a4d
Rahul <Rahul@kalpa.com>
2018-07-17 17:43:33 -0400

    Added messages.payload

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -15,10 +15,15 @@ class Exchange(object):
 		flow_state_ref = self.member_core_engine_ref.get_conv_flow_state(flow_state)
 		response_type = flow_state_ref.get().to_dict().get('response_type')
 		response = flow_state_ref.get().to_dict().get('response')
-		recipient = None
+		platformAction = flow_state_ref.get().to_dict().get('platformAction')
+		platformAction()
 		print("Contnuing a conversation. Flow state is {}".format(flow_state))
+		member_ref.add_expert(member,user_response)
+
 		if flow_state_ref.get().to_dict().get('recipient')== 'sender':
-			recipient = self.user_id_on_platform 
+			recipient = self.user_id_on_platform
+		else:
+			recipient = None
 		return message_payloads.fb_payload(response_type,response,recipient,conversation_ref.get().id)
 
 	def start_conversation(self,member_ref):
@@ -31,3 +36,6 @@ class Exchange(object):
 		if flow_state_ref.get().to_dict().get('recipient')== 'sender':
 			recipient = self.user_id_on_platform 
 		return message_payloads.fb_payload(response_type,response,recipient,conversation_ref.get().id)
+
+	def add_expert():
+		print("Calling def programmatically by name")
