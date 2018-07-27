bdb3e9cdab916ada3465deb7ac3603e428ee848a
Rahul <Rahul@kalpa.com>
2018-07-27 16:14:52 -0400

    Added responses.payload

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -14,7 +14,7 @@ class Exchange(object):
 
     def get_action(self, conversation_ref,conversation_state):
         print("Member Identifier is {} and conversation_ref is {} and conversation_state is {}".format(self.user_id_on_platform,conversation_ref.get().id, conversation_state))
-        payload =response_payload.fb_payload(conversation_state,'...',self.user_id_on_platform,conversation_ref.get().id)
+        payload = response_payload.fb_payload(conversation_state,'...',self.user_id_on_platform,conversation_ref.get().id)
         if 'platform' in payload:
             platform_action = payload['platform'].get('action')
             payload = getattr(self, platform_action)(payload,conversation_ref)
