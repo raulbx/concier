31c78255a2cd3ea4e21a2825c6000ba6cc8e6305
Rahul <Rahul@kalpa.com>
2018-08-08 17:31:22 -0400

    Added scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,15 @@
 #This piece sends scheduled messages
 from apscheduler.schedulers.background import BackgroundScheduler
 
-scheduler = BackgroundScheduler()
+#scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job('interval', minutes=1)
+#@scheduler.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job will run every three minutes.')
 
-scheduler.start()
+#scheduler.start()
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(timed_job, 'interval', seconds=30)
+    scheduler.start()
