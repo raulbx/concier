71087995bbc67015414e3034b732a6f2b0dfc175
Rahul <Rahul@kalpa.com>
2018-08-15 15:30:22 -0400

    Added responses.payload

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -58,8 +58,8 @@ class Exchange(object):
         return payload
 
     def record_value_set_future_state(self,payload,conversation_ref):
-        conversation_ref.update({payload['platform'].get('product_category'):self.user_response})
-        conversation_ref.update({'conversation_state':payload['platform'].get('future_state')})
+        conversation_ref.update({payload['platform'].get('field'):self.user_response})
+        #conversation_ref.update({'conversation_state':payload['platform'].get('future_state')})
         conversation_ref.update({'lastactivedate':datetime.datetime.now()})
         payload['message']['text'] = Template(payload['message'].get('text')).safe_substitute(arg1=self.user_response)
         del payload['platform']
