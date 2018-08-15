611790628da1093df0d152a0bcd7d688f02f4782
Rahul <Rahul@kalpa.com>
2018-08-15 14:38:33 -0400

    Added responses.payload

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ def send_message(payload):
     return result
 
 def get_user_details(sender_id):
-
-    request_endpoint = 'https://graph.facebook.com/v3.1/{}?fields=first_name,last_name&access_token={}'.format(sender_id,ACCESS_TOKEN)
+    temp_access_token='EAAFRHrTy7U0BAC1Ozb5LsKR7Wx6P4LlZBRktW09Ehn35sEGGPD6nrnlRQRiQDOns61X4IB2ACqZAWlIpDnG1dKWCincCzzaMmZA4PacDXpBjZBaYZB2WrHZCEGAQUvSiEHn0h0EZAy5WMoIhKIW8oEmKnaeMdO8cZBuMk78y43YMdc2hgfjCYNZBOZBCbTSJsZBn1UZD'
+    request_endpoint = 'https://graph.facebook.com/v3.1/{}?fields=first_name,last_name&access_token={}'.format(sender_id,temp_access_token)
     print (request_endpoint)
     response = requests.post(request_endpoint)
     user_profile_json = response.json()
