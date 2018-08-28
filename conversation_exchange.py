7c330ff7643c78a97b2d567a82dee2b107969e24
Rahul <Rahul@kalpa.com>
2018-08-28 06:36:43 -0400

    Added responses.payload

--- conversation_exchange.py
+++ conversation_exchange.py
@@ -134,7 +134,7 @@ class Exchange(object):
         else:
             if payload['platform'].get('helper_next_state'):
                 next_state = 'helper_next_state'
-                self.add_expertise(self,payload,conversation_ref)
+                self.add_expertise(payload,conversation_ref)
             elif payload['platform'].get('helpee_next_state'):
                 next_state = 'helpee_next_state'
             payload = response_payload.fb_payload(payload['platform'][next_state],'...',self.user_id_on_platform,conversation_ref.get().id,payload)
